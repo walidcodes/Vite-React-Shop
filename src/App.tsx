8c5df18408e394765cc9605461872e3c351353b4
Walid Youssef <49271416+Night-Spider@users.noreply.github.com>
2024-01-30 07:26:03 +0200

    one hour mark

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Home } from "./pages/home.tsx";
+import { About } from "./pages/about.tsx";
+import { Store } from "./pages/store.tsx";
+
+function App() {
+  return (
+    <Container className="mb-4">
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/store" element={<Store />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </Container>
+  );
+}
+
+export default App;
