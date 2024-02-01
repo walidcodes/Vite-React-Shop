24fd9bcbe2dd7ac0c67b7307e49e42724dbb6574
Walid Youssef <49271416+Night-Spider@users.noreply.github.com>
2024-02-01 07:13:36 +0200

    pre: store item populating

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import { Container } from "react-bootstrap";
 import { Home } from "./pages/home.tsx";
 import { About } from "./pages/about.tsx";
 import { Store } from "./pages/store.tsx";
+import { Navbar } from "./components/Navbar.tsx";
 
 function App() {
   return (
-    <Container className="mb-4">
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/store" element={<Store />}></Route>
-        <Route path="/about" element={<About />}></Route>
-      </Routes>
-    </Container>
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/store" element={<Store />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </Container>
+    </>
   );
 }
 
